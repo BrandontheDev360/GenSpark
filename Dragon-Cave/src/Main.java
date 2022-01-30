008e841679825f86f456943405816483be147a6c
Brandon <Brandonthedev360@gmail.com>
2022-01-29 22:28:50 -0500

    minor updates to dragon cave

--- Dragon-Cave/src/Main.java
+++ Dragon-Cave/src/Main.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("You are in a land full of dragons. " +
-                "In front of you, you see two caves. " +
-                "In one cave, the dragon is friendly and will share his treasure with you. " +
-                "The other dragon is greedy and hungry and will eat you on sight. Which Cave will you go in (0 , 1, 2)");
+        System.out.println("You are in a land full of dragons.\nIn front of you, you see two caves.\nIn one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which Cave will you go in (0 , 1, 2)");
         Scanner getInput = new Scanner(System.in);
         int input = Integer.parseInt(getInput.nextLine());
         if (input == 1) {
