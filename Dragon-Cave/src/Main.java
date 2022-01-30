572da4c9f83bca81617d9e7eb0974225c0df256a
Brandon <Brandonthedev360@gmail.com>
2022-01-29 22:37:23 -0500

    added loop for playing again

--- Dragon-Cave/src/Main.java
+++ Dragon-Cave/src/Main.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("You are in a land full of dragons.\nIn front of you, you see two caves.\nIn one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which Cave will you go in (0 , 1, 2)");
-        Scanner getInput = new Scanner(System.in);
-        int input = Integer.parseInt(getInput.nextLine());
-        if (input == 1) {
-            System.out.println("You entered cave 1 and have been eaten by the dragon");
-        } else if (input == 2) {
-            System.out.println("You entered cave 2 and have found treasure!");
-        } else if (input == 0) {
-            System.out.println("You exited the cave.");
+        boolean toggleWinOrLose = false;
+        String playAgain = "";
+        while(!toggleWinOrLose) {
+            System.out.println("You are in a land full of dragons.\nIn front of you, you see two caves.\nIn one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which Cave will you go in (0 , 1, 2)");
+            Scanner getInput = new Scanner(System.in);
+//        int input = Integer.parseInt(getInput.nextLine()); also works
+            int input = getInput.nextInt();
+            if (input == 1) {
+                System.out.println("You entered cave 1 and have been eaten by the dragon\nWould you like to play again? (yes or no)");
+            } else if (input == 2) {
+                System.out.println("You entered cave 2 and have found treasure!\nWould you like to play again? (yes or no)");
+            } else if (input == 0) {
+                System.out.println("You exited the cave.");
+            }
+            playAgain = getInput.next();
+            if (!playAgain.equals("yes")) {
+                toggleWinOrLose = true;
+            } else {
+                toggleWinOrLose = false;
+            }
+
         }
 
     }
