4942633fe9b0fed01f249c92371d999b6b1c9eba
Brandon <Brandonthedev360@gmail.com>
2022-02-01 11:28:00 -0500

    updates

--- Dragon-Cave/src/java/DragonCave.java
+++ Dragon-Cave/src/java/DragonCave.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class DragonCave {
-
+    // Variables
     // helps with scope and toggling while loop on and off.
     // SUPER USEFUL
     private static boolean toggle = true;
 
+    // While loop to keep the game going until player wants to stop
+    // Try catch to catch exception cases where player's input is not an Integer.
     public static void main(String[] args) {
         while(toggle){
             Scanner getInput = new Scanner(System.in);
@@ -19,10 +21,13 @@ public class DragonCave {
             }
         }
     }
+
+    // function to start game
     public static String startGame() {
         return "You are in a land full of dragons.\nIn front of you, you see two caves.\nIn one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight.\nWhich Cave will you go in (0 , 1, 2)";
     }
 
+    // function to getPlayer input and return correct output
     public static String playerInput(int input) {
         if (input == 1) {
             return "You entered cave 1 and have been eaten by the dragon!\nWould you want to play again? (yes or no)";
@@ -33,6 +38,8 @@ public class DragonCave {
         }
     }
 
+    // function to determine if player wants to play again
+    // also really cool how it can change toggle out of scope with the private static
     public static void playAgain(String playAgain) {
         if (playAgain.equals("yes")) {
             toggle = true;
