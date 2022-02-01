61c351a65df21c9781ca184759d86c5da1a668ae
Brandon <Brandonthedev360@gmail.com>
2022-02-01 12:14:55 -0500

    junit added to Dragon Cave and tests added

---
+++ Dragon-Cave/src/test/DragonCaveTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+
+public class DragonCaveTest {
+    DragonCave dragonCave;
+
+    @BeforeEach
+    void setUp() {
+        dragonCave = new DragonCave();
+    }
+
+    @Test
+    public void startGame() {
+        assertEquals("You are in a land full of dragons.\nIn front of you, you see two caves.\nIn one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight.\nWhich Cave will you go in (0 , 1, 2)", DragonCave.startGame(), "You did something wrong!" );
+    }
+
+    @Test
+    public void playerInput() {
+        assertAll("Different Caves",
+            () -> assertEquals("You entered cave 1 and have been eaten by the dragon!\nWould you want to play again? (yes or no)", DragonCave.playerInput(1), "Wrong Input"),
+            () -> assertEquals("You entered cave 2 and have found treasure!\nWould you want to play again? (yes or no)", DragonCave.playerInput(2), "Wrong Input"),
+            () -> assertEquals("You exited the cave.\nWould you want to play again? (yes or no)", DragonCave.playerInput(0), "Wrong Input"));
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+}
