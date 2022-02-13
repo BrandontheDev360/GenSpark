f52369b1a6a6d2e70a5b023afecfcd3bd033829b
Brandon <Brandonthedev360@gmail.com>
2022-02-13 18:46:46 -0500

    minor fixes

--- Hangman Game/src/java/Hangman.java
+++ Hangman Game/src/java/Hangman.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,14 +8,14 @@ public class Hangman {
     public static void main (String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("C:/GenSpark/Hangman Game/supersecretword.txt"));
         Scanner getInput = new Scanner(System.in);
-        List<String> wordList = new ArrayList<>();
+        ArrayList<String> wordList = new ArrayList<>();
         while(scanner.hasNext()) {
             wordList.add(scanner.nextLine());
         }
         Random rand = new Random();
         String hangManWord = wordList.get(rand.nextInt(wordList.size()));
         System.out.println(hangManWord);
-        List<Character> guesses = new ArrayList<>();
+        ArrayList<Character> guesses = new ArrayList<>();
         boolean toggleWinOrLose = true;
         while (toggleWinOrLose) {
             playerGuess(getInput, hangManWord, guesses);
@@ -25,7 +24,7 @@ public class Hangman {
         }
     }
 
-    public static boolean printWordState(String hangManWord, List<Character> guesses) {
+    public static boolean printWordState(String hangManWord, ArrayList<Character> guesses) {
         int correctGuess = 0;
         for (int i = 0; i < hangManWord.length(); i++) {
             if (guesses.contains(hangManWord.charAt(i))) {
@@ -39,7 +38,7 @@ public class Hangman {
         return (hangManWord.length() == correctGuess);
     }
 
-    public static void playerGuess(Scanner getInput, String hangManWord, List<Character> guesses) {
+    public static void playerGuess(Scanner getInput, String hangManWord, ArrayList<Character> guesses) {
         System.out.println("Please guess a letter.");
         String letterGuess = getInput.nextLine();
         guesses.add(letterGuess.charAt(0));
